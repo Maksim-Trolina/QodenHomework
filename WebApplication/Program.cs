258f9d7c63f3d184a210957bfafe72cf7357d867
Maksim <maks.panasenko01@bk.ru>
2020-12-09 01:38:23 +0300

    Add WebHostBuilder

--- WebApplication/Program.cs
+++ WebApplication/Program.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore;
 using Microsoft.Extensions.Logging;
 
 namespace WebApplication
@@ -16,8 +17,8 @@ namespace WebApplication
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+        public static IWebHostBuilder CreateHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
     }
 }
